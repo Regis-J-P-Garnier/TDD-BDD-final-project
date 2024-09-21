3a5586c4bb99c593461552f9456fba541932bce8
RJPG <regis.garnier@wanadoo.fr>
2024-09-21 10:47:07 +0200

    add webdriver manager for CHROME

--- features/environment.py
+++ features/environment.py
@@ -3,21 +3,21 @@ Environment for Behave Testing
 """
 from os import getenv
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install()) 
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '30'))
-BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
-DRIVER = getenv('DRIVER', 'firefox').lower()
+BASE_URL = getenv('BASE_URL', 'http://localhost:8081')
+DRIVER = getenv('DRIVER', 'chrome').lower()
 
+# features/environment.py
+#from selenium import webdriver
 
 def before_all(context):
     """ Executed once before all tests """
     context.base_url = BASE_URL
     context.wait_seconds = WAIT_SECONDS
-    # Select either Chrome or Firefox
-    if 'firefox' in DRIVER:
-        context.driver = get_firefox()
-    else:
-        context.driver = get_chrome()
+    context.driver = driver    
     context.driver.implicitly_wait(context.wait_seconds)
     context.config.setup_logging()
 
@@ -25,22 +25,4 @@ def before_all(context):
 def after_all(context):
     """ Executed after all tests """
     context.driver.quit()
-
-######################################################################
-# Utility functions to create web drivers
-######################################################################
-
-def get_chrome():
-    """Creates a headless Chrome driver"""
-    options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
-    return webdriver.Chrome(options=options)
-
-
-def get_firefox():
-    """Creates a headless Firefox driver"""
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    return webdriver.Firefox(options=options)    
     
